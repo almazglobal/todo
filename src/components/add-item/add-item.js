67906ccf39da1825f2d671796b01c7dc58a7deed
Alex <sasha_samara@bk.ru>
2020-10-31 00:15:19 +0400

    Refactoring and add form for adding items

--- src/components/add-item/add-item.js
+++ src/components/add-item/add-item.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './add-item.css';
 
-const AddItem = ({ onAdd }) => {
-  return (
-    <div className="d-flex add-item">
-      <input
-        className="add-panel"
-        type="text"
-        placeholder="What needs to be done?"
-      />
-      <button
-        type="button"
-        className="btn btn-outline-secondary"
-        onClick={() => onAdd('New Day!!')}
-      >
-        Add
-      </button>
-    </div>
-  );
-};
-
-export default AddItem;
+export default class AddItem extends Component {
+  state = {
+    nameItem: '',
+  };
+  onLabelChange = (evt) => {
+    this.setState({ nameItem: evt.target.value });
+  };
+  onSubmit = (evt) => {
+    evt.preventDefault();
+    this.props.onAdd(this.state.nameItem);
+    this.setState({ nameItem: '' });
+  };
+  render() {
+    return (
+      <form className="d-flex add-item" onSubmit={this.onSubmit}>
+        <input
+          className="form-control add-panel"
+          type="text"
+          placeholder="What needs to be done?"
+          onChange={this.onLabelChange}
+          value={this.state.nameItem}
+        />
+        <button className="btn btn-outline-secondary">Add</button>
+      </form>
+    );
+  }
+}
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -9,9 +9,9 @@ import './app.css';
 export default class App extends Component {
   state = {
     todoData: [
-      { label: 'Drink Coffee', important: false, id: 1 },
-      { label: 'Make Awesome App', important: true, id: 2 },
-      { label: 'Have a lunch', important: false, id: 3 },
+      { label: 'Drink Coffee', important: false, done: false, id: 1 },
+      { label: 'Make Awesome App', important: true, done: false, id: 2 },
+      { label: 'Have a lunch', important: false, done: false, id: 3 },
     ],
   };
   deleteItem = (id) => {
@@ -25,7 +25,6 @@ export default class App extends Component {
   generateID = (todoData) => {
     if (todoData.length === 0) return 1;
     const arrID = todoData.map((el) => el.id);
-    console.log(arrID);
     return Math.max(...arrID) + 1;
   };
   addItem = (text) => {
@@ -39,17 +38,53 @@ export default class App extends Component {
     });
   };
 
+  toggleProperty = (arr, id, propName) => {
+    const idx = arr.findIndex((el) => el.id === id);
+    const oldItem = arr[idx];
+    const item = { ...arr[idx], [propName]: !oldItem[propName] };
+    const before = arr.slice(0, idx);
+    const after = arr.slice(idx + 1);
+    return [...before, item, ...after];
+  };
+
+  onToggleImportant = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'important'),
+      };
+    });
+  };
+
+  onToggleDone = (id) => {
+    this.setState(({ todoData }) => {
+      return {
+        todoData: this.toggleProperty(todoData, id, 'done'),
+      };
+    });
+  };
+
+  countDone = (arr) => {
+    return arr.reduce((acc, el) => (el.done ? ++acc : acc), 0);
+  };
+
   render() {
     const { todoData } = this.state;
+    const done = this.countDone(todoData);
+    const toDo = todoData.length - done;
     return (
       <div className="todo-app">
-        <AppHeader toDo={3} done={1} />
+        <AppHeader toDo={toDo} done={done} />
         <div className="top-panel d-flex">
           <SearchPanel />
           <ItemStatusFilter />
         </div>
 
-        <TodoList todos={todoData} onDeleted={this.deleteItem} />
+        <TodoList
+          todos={todoData}
+          onDeleted={this.deleteItem}
+          onToggleImportant={this.onToggleImportant}
+          onToggleDone={this.onToggleDone}
+        />
         <AddItem onAdd={this.addItem} />
       </div>
     );
